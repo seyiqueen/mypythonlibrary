e584e58b581fc470c30b9542c229e7eb35cf2be1
unknown <sodiposeyi@gmail.com>
2023-03-13 11:15:58 +0000

    Complete development of the library to get the distance between two GPS points using Haversine formula and creating test function for it.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='mypythonlib',
+    packages=find_packages(include=['mypythonlib']),
+    version='0.1.0',
+    description='My first Python library',
+    author='Me',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
